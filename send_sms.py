2650a9cc0aea38e56ea14761eb7ac539d0defd09
Tyrique <tvanwyk114@gmail.com>
2020-12-09 00:34:05 +0200

    Completed add new birthday module

--- send_sms.py
+++ send_sms.py
@@ -1,13 +1,13 @@
 from twilio.rest import Client 
- 
-account_sid = 'ACc057ab3c92cf4e70e48f1a206944378d' 
-auth_token = 'be9a68a0f844e99a3d25030cc589525e' 
-client = Client(account_sid, auth_token) 
- 
-message = client.messages.create( 
+
+def send_message:
+	account_sid = 'ACc057ab3c92cf4e70e48f1a206944378d' 
+	auth_token = 'be9a68a0f844e99a3d25030cc589525e' 
+	client = Client(account_sid, auth_token) 
+	message= client.messages.create(
                               from_='whatsapp:+14155238886',  
                               body='Your appointment is coming up on July 21 at 3PM',      
                               to='whatsapp:+27627004903' 
                           ) 
  
-print(message.sid)
+	print(message.sid)
