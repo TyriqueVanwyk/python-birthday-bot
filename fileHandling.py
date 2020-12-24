6169575298a284a6623b4c062d74bd696474405f
Tyrique <tvanwyk114@gmail.com>
2020-12-24 17:23:17 +0200

    made the file handling file a class, still has some issue

--- fileHandling.py
+++ fileHandling.py
@@ -1,78 +1,84 @@
 import sys
 import json
-def addBirthday():
-	print("")
-	print("----------------------------------------------------------------------------------------------------------------------------------------------")
-	print("")
-	print("Type '0' if you want to cancel and quit")
-	print("Person contact Name:")
-	contactName = input()
-	#Check if the user wants to quit
-	if contactName == "0": sys.exit()
-	print("Contact number:")
-	contactNumber = input()
-	#Check if the user wants to quit
-	if contactNumber == "0": sys.exit()
-	print("Contact birth date (mm/dd format):")
-	contactBirthday = input() ## mm/dd format
-	#Check if the user wants to quit
-	if contactBirthday== "0":sys.exit()
-	#Creates format for json file
-	data = {}
-	data['people'] = []
-	data['people'].append({
-		'name' : contactName,
-		'number' : contactNumber,
-		'birthdate' : contactBirthday,
-	})
-	##Creates a new file if the file does not exist and if the file does exist dumps all the json info into the file
-	with open("Birthdays.txt", "a") as outfile:
-		 json.dump(data, outfile)
+class FileHandlingClass:
 
-def menu():
-	print("-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-")
-	print("						iS The brithday messanger for us who forget birthdays :)")
-	print("-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_")
-	print("Select a item from the menu")
-	print("1. Add New Birthday")
-	print("2. Edit Birthdays")
-	print("0. Exit")
+    def __init__(self, _fileName):
+        self.fileName = _fileName
+    
 
-	userChoice = input()
-	return userChoice
+    def addBirthday(self):
+        print("")
+        print("----------------------------------------------------------------------------------------------------------------------------------------------")
+        print("")
+        print("Type '0' if you want to cancel and quit")
+        print("Person contact Name:")
+        contactName = input()
+        #Check if the user wants to quit
+        if contactName == "0": sys.exit()
+        print("Contact number:")
+        contactNumber = input()
+        #Check if the user wants to quit
+        if contactNumber == "0": sys.exit()
+        print("Contact birth date (mm/dd format):")
+        contactBirthday = input() ## mm/dd format
+        #Check if the user wants to quit
+        if contactBirthday== "0":sys.exit()
+        #Creates format for json file
+        data = {}
+        data['people'] = []
+        data['people'].append({
+            'name' : contactName,
+            'number' : contactNumber,
+            'birthdate' : contactBirthday,
+        })
+        ##Creates a new file if the file does not exist and if the file does exist dumps all the json info into the file
+        with open("Birthdays.txt", "a") as outfile:
+             json.dump(data, outfile)
 
-##This function is to make the process of the 'Add new birthday menu' usable in other files
-def menuNewBirthday():
-	newBday = True
-	addBirthday()
-	while newBday:
-		print("Do you wish to continue? ('y' for yes and 'n' for no and type '0' to exit)")
-		userinput = input()
-		if userinput.upper() == "Y":
-			newBday = True
-		elif userinput.upper() == "N":
-			newBday = False
-			exit
-		elif userinput.upper()=="0":
-			newBday = False
-			menu()
-			exit
+    def menu(self):
+        print("-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-")
+        print("						iS The brithday messanger for us who forget birthdays :)")
+        print("-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_")
+        print("Select a item from the menu")
+        print("1. Add New Birthday")
+        print("2. Edit Birthdays")
+        print("0. Exit")
 
-###Reading from file using method
-def readContacts(filename):
-    with open(filename, "a") as f:
-        data = json.load(f)
-    return data
+        userChoice = input()
+        return userChoice
 
-## use the menu
-try:
-	userChoice = menu()
-	if userChoice == "1":
-		##call the function to execute the menu
-		menuNewBirthday()
+    ##This function is to make the process of the 'Add new birthday menu' usable in other files
+    def menuNewBirthday(self):
+        newBday = True
+        self.addBirthday()
+        while newBday:
+            print("Do you wish to continue? ('y' for yes and 'n' for no and type '0' to exit)")
+            userinput = input()
+            if userinput.upper() == "Y":
+                newBday = True
+            elif userinput.upper() == "N":
+                newBday = False
+                exit
+            elif userinput.upper()=="0":
+                newBday = False
+                self.menu()
+                exit
 
-	elif userChoice == "0":
-		exit
-except:
-	print("An error occured")
+    ###Reading from file using method
+    def readContacts(self):
+        with open(self.fileName, "a") as f:
+            data = json.load(f)
+        return data
+    def useMenu(self):
+        ## use the menu
+        try:
+            userChoice = self.menu()
+            if userChoice == "4":
+                ##call the function to execute the menu
+                self.menuNewBirthday()
+                
+            elif userChoice == "3":
+                exit
+        except Exception as e:
+            print(e)
 
